1b187423dde37202d6ad3a4054a05c1c994dd777
visualstudio <visualstudio@122-PC38>
2019-01-18 21:26:47 -0500

    bsl

--- c0731902/Program.cs
+++ c0731902/Program.cs
@@ -6,12 +6,30 @@ using System.Threading.Tasks;
 
 namespace c0731902
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
-            int i; i = 1;
-            Console.WriteLine(i + 1);
+            StudentInformation _Mary = new StudentInformation("Mary", 3.5, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 2.8, 18);
+            Console.ReadLine();
+        }
+    }
+    class StudentInformation
+    {
+        public string Name;
+
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(string aName, double cGPA, int dAttendance)
+        {
+            Name = aName;
+
+            GPA = cGPA;
+            Attendance = dAttendance;
         }
     }
 }
